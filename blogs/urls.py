6ba8e782730108ff65e66388edf1af7d7ec9e44b
Clebson Carvalho <clebson2007.farias@gmail.com>
2017-02-10 14:50:51 -0300

    Finalizei a primeira parte

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from . import views
 
+
 urlpatterns = [
     url(r'^$', views.posts, name='posts'),
     url(r'^novoPost/$', views.novoPost, name='novoPost'),
     url(r'^editPost/(?P<post_id>\d+)/$', views.editPost, name='editPost'),
+	url(r'^detailPost/(?P<post_id>\d+)/$', views.detailPost, name='detailPost'),
 ]
